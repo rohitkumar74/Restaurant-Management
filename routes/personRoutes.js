3aed19d27826fac41c77738a9bb71c9b101bd5dc
Rohit <rohit7463843025@gmail.com>
2024-02-13 18:08:44 +0530

    new changes commit

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const person = require('./../models/person');
+const person = require('../models/person');
 
 // Post route to add a person
 router.post('/', async(req, res) => {
